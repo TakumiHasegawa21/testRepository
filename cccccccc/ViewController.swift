0775d918b96788d8cd20c5b3b4795d939156d8ea
Takumi Hasegawa <108503079+TakumiHasegawa21@users.noreply.github.com>
2022-10-17 10:53:39 +0900

    変更を加えました

--- cccccccc/ViewController.swift
+++ cccccccc/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        Unko.text = "aaaaaaa"
         // Do any additional setup after loading the view.
     }
 
